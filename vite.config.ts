8c609c3daa36405d37679a7b61419a74ddc53e97
Feybulous <fenja@famnilienese.de>
2025-08-27 10:25:37 +0200

    fix: relative base path for GH Pages

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,8 @@
+// vite.config.ts
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
 export default defineConfig({
-  base: '/address-splitter/',
   plugins: [react()],
+  base: '/address-splitter/', // muss exakt Repo-Name matchen, case-sensitive
 });
